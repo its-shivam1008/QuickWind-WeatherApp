6c9a15779633e56cf9c6be5702044880cb9896ed
Shivam Shukla <tanil5960@gmail.com>
2024-04-14 14:09:07 +0530

    nav up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <header className='text-white absolute w-[100%] z-[9999] px-[0.9rem] py-[0] top-0 left-0'>
       <nav className=' max-w-[1300px] my-0 mx-auto py-0 px-auto flex justify-between items-center'>
-        <Link to="/Home"><div className='text-4xl  hover:text-[#dcf13b] tracking-wide  leading-tight text-shadow-md font-bold'>
+        <Link to="/"><div className='text-4xl  hover:text-[#dcf13b] tracking-wide  leading-tight text-shadow-md font-bold'>
           QuickWind
         </div></Link>
         <ul className='flex  space-x-2 md:space-x-5 py-3'>
